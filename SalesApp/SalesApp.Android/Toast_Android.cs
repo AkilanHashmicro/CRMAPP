0e7a8a46d5ee43a08829a471ba4db11c5a15edba
AkilanHashmicro <akilan.hashmicro@gmail.com>
2020-07-01 13:16:12 +0530

    Final commit

---
+++ SalesApp/SalesApp.Android/Toast_Android.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SalesApp.Droid;
+using SalesApp.views;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Toast_Android))]
+namespace SalesApp.Droid
+{
+    public class Toast_Android : Toast
+    {
+        public void Show(string message)
+        {
+            Android.Widget.Toast.MakeText(Android.App.Application.Context, message, Android.Widget.ToastLength.Long).Show();
+        }
+    }
+}
+
