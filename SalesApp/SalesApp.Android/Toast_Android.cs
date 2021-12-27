d6f801cb237d26d19f5577320cc847a77a8fb814
AkilanHashmicro <akilan.hashmicro@gmail.com>
2021-12-27 12:55:28 +0530

    Full path commit

---
+++ SalesApp/SalesApp.Android/Toast_Android.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SalesApp.Droid
+{
+    public class Toast_Android : ContentPage
+    {
+        public Toast_Android()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
